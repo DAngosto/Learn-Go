871ebae0e08c55f65a35b1dabddaac15fd9bb6f7
dangosto <daniel.angosto.martinez@gmail.com>
2019-01-10 16:01:43 +0100

    Entendiendo la estructura de control If

--- Estructuras_de_Control/main.go
+++ Estructuras_de_Control/main.go
@@ -23,4 +23,22 @@ func main() {
 		break
 	}
 
+	//Estructuras de Control: if
+
+	// if 5 < 6 {
+	// 	fmt.Println("5 es menor que 6")
+	// }
+
+	a := 6
+
+	if a < 6 {
+		fmt.Println("a es menor que 6")
+	} else if a > 6 {
+		fmt.Println("a es mayor que 6")
+	} else {
+		fmt.Println("a es igual 6")
+	}
+
+	fmt.Println("Fin del programa")
+
 }
